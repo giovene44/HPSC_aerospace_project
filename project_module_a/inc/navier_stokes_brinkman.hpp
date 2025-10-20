ed126ac265c3466e9b8ac6a42d2141f50e2fe70c
FraVirgu <francesco.virgulti@mail.polimi.it>
2025-10-20 11:52:22 +0200

    create method solve linear

--- project_module_a/inc/navier_stokes_brinkman.hpp
+++ project_module_a/inc/navier_stokes_brinkman.hpp
@@ -3,74 +3,87 @@
 #include "VectorVariable.hpp"
 #include "gamma_beta.h"
 
-class NavierStokesBrinkmann {
+class NavierStokesBrinkmann
+{
 public:
-    class Function{
+    class Function
+    {
     public:
-        Function(){}
+        Function() {}
         float value(/*Point*/){};
     };
 
-    class Porosity{}; // TODO: Discuss in case the porosity is in a file or is a given function
+    class Porosity
+    {
+    }; // TODO: Discuss in case the porosity is in a file or is a given function
 
-    class ForcingTerm : public Function{
+    class ForcingTerm : public Function
+    {
     public:
-        ForcingTerm(){}
-        float value(int i, int j, int k, float t){ return 0.0; } // Placeholder implementation
+        ForcingTerm() {}
+        float value(int i, int j, int k, float t) { return 0.0; } // Placeholder implementation
     };
 
-    
-    class Nu: public Function{};
+    class Nu : public Function
+    {
+    };
 
-    class Grid{
+    class Grid
+    {
 
-    // Until we get more information for this part we will leave it empty
+        // Until we get more information for this part we will leave it empty
     public:
-        Grid(){}
+        Grid() {}
 
-        void compute_grid(){};
-        void split_in_blocks(){};
+        void compute_grid() {};
+        void split_in_blocks() {};
         void compute_porosity(/*data structure for porosity*/){};
 
-        protected:
+    protected:
     };
 
     NavierStokesBrinkmann();
     void solve();
+
 protected:
+    // setup methods:
+    void setup(); // read grid, setup initial values, assemble K, gamma, initial conditions
 
-    //setup methods:
-    void setup(); //read grid, setup initial values, assemble K, gamma, initial conditions
-
-    //methods inside the iteration:
-    void assemble_g_rhs();
+    // methods inside the iteration:
+    void assemble_g();
+    auto assemble_rhs(auto &vector_1, auto &vector_2);
+    void solve_linear_systems(auto &rhs, auto &a, auto &b, auto &c, auto &output);
+    void update_variables();
     void solve_momentum();
     void solve_pressure();
     void output_results(int timestep) const;
-
 
     Grid grid;
     Porosity porosity;
     ForcingTerm forcing_term;
 
-    //Some of these data structures can be merged in one with further optimization
+    // Some of these data structures can be merged in one with further optimization
 
     ScalarVariables pressure_predictor;
     VectorVariable velocity_predictor;
     ScalarVariables psi;
     ScalarVariables phi;
     ScalarVariables other_phi;
+    ScalarVariables rhs;
     VectorVariable g_rhs; // rhs of the momentum equation
     VectorVariable eta;
     VectorVariable zeta;
     VectorVariable xi;
     VectorVariable nu;
+    ScalarVariables sol_linear_system;
+    ScalarVariables a;
+    ScalarVariables b;
+    ScalarVariables c;
     Beta beta;
     Gamma gamma;
     Dim Nx;
     Dim Ny;
     Dim Nz;
-
 
     VectorVariable velocity_solution;
     ScalarVariables pressure_solution;
--- project_module_a/src/navies_stokes_brinkman.cpp
+++ project_module_a/src/navies_stokes_brinkman.cpp
@@ -3,54 +3,63 @@
 
 void NavierStokesBrinkmann::assemble()
 
-
-void NavierStokesBrinkmann::assemble_g_rhs()
+    void NavierStokesBrinkmann::assemble_g()
 {
 
-    //this method assembles the g vector for the current time step.
+    // this method assembles the g vector for the current time step.
     //(g vector is the rhs of the momentum equation!)
 
     double t = 0.0; // Placeholder for current time, (TODO) should be set appropriately
 
-    for (int i=0; i<g_rhs.size(); i++){ //i indicates the component among the 3 components of the vector
-        for (int j=0; j<g_rhs[0].size(); j++) //j indicates the index of the element in the vector
+    for (int i = 0; i < g_rhs.size(); i++)
+    {                                             // i indicates the component among the 3 components of the vector
+        for (int j = 0; j < g_rhs[0].size(); j++) // j indicates the index of the element in the vector
         {
 
-            float eta_local_second_derivative = eta.second_derivative(i, 0, j); // x direction
+            float eta_local_second_derivative = eta.second_derivative(i, 0, j);   // x direction
             float zeta_local_second_derivative = zeta.second_derivative(i, 1, j); // y direction
-            float u_local_second_derivative = u.second_derivative(i, 2, j); // z direction
+            float u_local_second_derivative = u.second_derivative(i, 2, j);       // z direction
 
-            g_rhs[i][j] = forcing_term.value(i,j,t) - gradient_pressure[i][j] 
-            - eta*velocity_predictor[i][j]/(2.0* /*K[i][j] or K.get(i,j)*/)+ eta/2.0 
-            * (eta_local_second_derivative + zeta_local_second_derivative + u_local_second_derivative);
-
+            g_rhs[i][j] = forcing_term.value(i, j, t) - gradient_pressure[i][j] - eta * velocity_predictor[i][j] / (2.0 * /*K[i][j] or K.get(i,j)*/) + eta / 2.0 * (eta_local_second_derivative + zeta_local_second_derivative + u_local_second_derivative);
         }
     }
 }
 
-void NavierStokesBrinkmann::solve_momentum()
+auto NavierStokesBrinkmann::assemble_rhs(auto output, auto vector1, auto vector2)
 {
-    // set the righ t-hand side vector xi
-    for (int a=0; a<3; a++){ //a indicates the component among the 3 components of the vector
-        for (int i=0; i<Nx; i++){ //i indicates the index of the element in the vector
-            for (int j=0; j<Ny; j++){
-                for(int k=0; k<Nz; k++){
-
-                    xi.set(a,i,j,k) = u.value(a,i,j,k) + dt / beta(i,j,k) * g_rhs.value(a,i,j,k) - eta.value(a,i,j,k);
-                
+    for (int a = 0; a < 3; a++)
+    { // a indicates the component among the 3 components of the vector
+        for (int i = 0; i < Nx; i++)
+        { // i indicates the index of the element in the vector
+            for (int j = 0; j < Ny; j++)
+            {
+                for (int k = 0; k < Nz; k++)
+                {
+                    output.set(a, i, j, k) = vector1.value(a, i, j, k) - vector2.value(a, i, j, k);
                 }
             }
         }
     }
-
-
-    
-
+}
 
 
 
 
+void NavierStokesBrinkmann::solve_momentum()
+{
+    // set the righ t-hand side vector xi
+    for (int a = 0; a < 3; a++)
+    { // a indicates the component among the 3 components of the vector
+        for (int i = 0; i < Nx; i++)
+        { // i indicates the index of the element in the vector
+            for (int j = 0; j < Ny; j++)
+            {
+                for (int k = 0; k < Nz; k++)
+                {
 
-
-
+                    xi.set(a, i, j, k) = u.value(a, i, j, k) + dt / beta(i, j, k) * g_rhs.value(a, i, j, k);
+                }
+            }
+        }
+    }
 }
